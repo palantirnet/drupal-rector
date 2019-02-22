e6d42303bbac33259c515fceaf1d73a12e940acc
Dezső BICZÓ <mxr576@gmail.com>
2019-02-22 15:05:30 +0100

    Rector that fixes a regular issue caused by traits.
    Have you ever got an error caused by calling
    ->addStatus() on null?

---
+++ src/Property/PropertyToMethodCallRector.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mxr576\Rector\Property;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\PropertyFetch;
+use PhpParser\Node\Identifier;
+use Rector\NodeTypeResolver\Node\Attribute;
+use Rector\Rector\AbstractRector;
+use Rector\RectorDefinition\ConfiguredCodeSample;
+use Rector\RectorDefinition\RectorDefinition;
+
+final class PropertyToMethodCallRector extends AbstractRector
+{
+    /**
+     * @var string[][]
+     */
+    private $propertyToMethod = [];
+
+    /**
+     * PropertyToMethodCallRector constructor.
+     *
+     * @param array $propertyToMethod
+     */
+    public function __construct(array $propertyToMethod)
+    {
+        $this->propertyToMethod = $propertyToMethod;
+    }
+
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Replaces method calls on a property with a method call on a method.', [
+            new ConfiguredCodeSample(
+                <<<'CODE_SAMPLE'
+$this->messenger->addStatus($this->t('Foo bar'));
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+$this->messenger()->addStatus($this->t('Foo bar'));
+CODE_SAMPLE
+                ,
+                [
+                    '$propertyToMethod' => [
+                        'Drupal\Core\Messenger\MessengerTrait' => [
+                            'messenger' => 'messenger',
+                        ],
+                    ],
+                ]
+            ), ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [PropertyFetch::class];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function refactor(Node $node): ?Node
+    {
+        /* @var \PhpParser\Node\Expr\PropertyFetch $node **/
+        foreach ($this->propertyToMethod as $type => $propertyToMethod) {
+            if ($this->isType($node->getAttribute(Attribute::CLASS_NODE), $type)) {
+                if (array_key_exists($node->name->name, $propertyToMethod)) {
+                    // Ignore non method calls.
+                    if (null === $node->getAttribute(Attribute::NEXT_NODE) || !$node->getAttribute(Attribute::NEXT_NODE) instanceof Identifier) {
+                        continue;
+                    }
+
+                    return $this->createMethodCall($node->var, $propertyToMethod[$node->name->name], []);
+                }
+            }
+        }
+
+        return null;
+    }
+}
