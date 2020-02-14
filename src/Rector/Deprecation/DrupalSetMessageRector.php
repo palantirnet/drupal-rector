5b3879563fa2756fa775ea170b3df6271ba7e717
Ofer Shaal <shaal@palantir.net>
2020-02-14 12:03:22 -0500

    updating changed rector file location

--- src/Rector/Deprecation/DrupalSetMessageRector.php
+++ src/Rector/Deprecation/DrupalSetMessageRector.php
@@ -5,9 +5,9 @@ namespace Drupal8Rector\Rector\Deprecation;
 use Drupal8Rector\Utility\TraitsByClassHelperTrait;
 use PhpParser\Node;
 use Rector\NodeTypeResolver\Node\AttributeKey;
-use Rector\Rector\AbstractRector;
-use Rector\RectorDefinition\CodeSample;
-use Rector\RectorDefinition\RectorDefinition;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\RectorDefinition\CodeSample;
+use Rector\Core\RectorDefinition\RectorDefinition;
 
 /**
  * Replaces deprecated drupal_set_message() calls.
