7929e6f74080fd554f71f8adb2c4cce96ac88c49
Daniel Montgomery <damontgomery@gmail.com>
2020-03-21 12:28:29 -0500

    entityManager will now attempt to update controllers.

--- rector_examples/src/Controller/EntityManagerController.php
+++ rector_examples/src/Controller/EntityManagerController.php
@@ -7,12 +7,27 @@ use Drupal\Core\Controller\ControllerBase;
 class EntityManagerController extends ControllerBase {
 
   /**
-   * Example of static method calls from a class.
+   * Example of method calls from a class.
    *
    * @return null
    */
   public function example() {
+    // Setting the service to the local variable.
     $entity_manager = $this->entityManager();
+
+    // Using a method on the service directly.
+    $definitions = $this->entityManager()->getDefinitions();
+
+    // Using methods that are not in the entityTypeManager service.
+    $entity_manager = $this->entityManager();
+
+    $group = FALSE;
+    $class_name = 'MyClass';
+
+    $entity_manager->getEntityTypeLabels($group);
+    $entity_manager->getEntityTypeFromClass($class_name);
+
+    $entity_manager = $this->entityManager()->getEntityTypeLabels($group);
 
     return NULL;
   }
