fe25f489b6d9f37591f5e259d2bba273a4383cec
Ofer Shaal <shaal@palantir.net>
2020-04-09 18:23:24 -0400

    updating index, adding comments to rector_examples

--- rector_examples/test/src/Functional/BrowserTestBaseGetMock.php
+++ rector_examples/test/src/Functional/BrowserTestBaseGetMock.php
@@ -8,14 +8,14 @@ use Drupal\Tests\BrowserTestBase;
 class BrowserTestBaseGetMock extends BrowserTestBase {
 
   /**
-   *
+   * An example that was copied from the change record.
    */
   public function simple_example() {
       $this->entityTypeManager = $this->getMock(EntityTypeManagerInterface::class);
   }
 
   /**
-   *
+   * An example similar to the one above but more commonly used
    */
   public function class_name_as_string() {
     $this->entityTypeManager = $this->getMock('Drupal\Core\Entity\EntityTypeManagerInterface');
--- rector_examples/test/src/Functional/BrowserTestBaseGetMockUpdated.php
+++ rector_examples/test/src/Functional/BrowserTestBaseGetMockUpdated.php
@@ -8,14 +8,14 @@ use Drupal\Tests\BrowserTestBase;
 class BrowserTestBaseGetMockUpdated extends BrowserTestBase {
 
   /**
-   *
+   * An example that was copied from the change record.
    */
   public function simple_example() {
       $this->entityTypeManager = $this->createMock(EntityTypeManagerInterface::class);
   }
 
   /**
-   *
+   * An example similar to the one above but more commonly used
    */
   public function class_name_as_string() {
     $this->entityTypeManager = $this->createMock('Drupal\Core\Entity\EntityTypeManagerInterface');
