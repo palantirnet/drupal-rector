38e0beb1b73323d35df961389b5e5dd84272ba3b
Daniel Montgomery <damontgomery@gmail.com>
2020-04-13 14:58:22 -0500

    format_date rector.

---
+++ rector_examples/src/FormatDateStaticUpdated.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\rector_examples;
+
+/**
+ * Example of updated static method calls from a class.
+ */
+class FormatDateStaticUpdated {
+
+  /**
+   * A simple example using the minimum number of arguments.
+   */
+  public function simple_example() {
+    $formatted_date = \Drupal::service('date.formatter')->format('123456');
+  }
+
+  /**
+   * An example using all of the arguments.
+   */
+  public function using_all_arguments() {
+    $formatted_date = \Drupal::service('date.formatter')->format(time(), 'custom', 'Y M D', 'America/Los_Angeles', 'en-us');
+  }
+
+}
