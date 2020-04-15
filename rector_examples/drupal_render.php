55ae50350201e6c1935eac42569b36f38313067d
Ofer Shaal <shaal@palantir.net>
2020-04-15 12:01:14 -0400

    adding drupal_render example

---
+++ rector_examples/drupal_render.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This demonstrates the deprecated static calls that might be called from procedural code like `.module` files.
+ */
+
+/**
+ * A simple example using the minimum number of arguments.
+ */
+function simple_example() {
+  drupal_render($elements);
+}
+
+/**
+ * An example using all of the arguments.
+ */
+function using_all_arguments() {
+  $is_recursive_call = false;
+  drupal_render($elements, $is_recursive_call);
+}
+
