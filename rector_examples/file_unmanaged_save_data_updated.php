cc1980f62e36accd5062f4b30a7c3c4164ab3c38
Kelsey Bentham <bentham@palantir.net>
2020-04-17 12:06:27 -0500

    Add rector for the deprecated file_unmanaged_save_data() function.

---
+++ rector_examples/file_unmanaged_save_data_updated.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This demonstrates the deprecated static calls that might be called from procedural code like `.module` files.
+ */
+
+/**
+ * A simple example using the minimum number of arguments.
+ */
+function simple_example() {
+  \Drupal::service('file_system')->saveData('example');
+}
+
+/**
+ * An example using all of the arguments.
+ */
+function using_all_arguments() {
+  $snippet = 'example';
+  $destination = "public://test/test.txt";
+
+  \Drupal::service('file_system')->saveData($snippet, $destination, \Drupal\Core\File\FileSystemInterface::EXISTS_REPLACE);
+}
+
+/**
+ * This shows using a variable as the options.
+ */
+function options_as_variable() {
+  $snippet = 'example';
+  $destination = "public://test/test.txt";
+  $options = \Drupal\Core\File\FileSystemInterface::EXISTS_REPLACE;
+
+  \Drupal::service('file_system')->saveData($snippet, $destination, $options);
+}
