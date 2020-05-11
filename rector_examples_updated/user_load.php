136ece3cf9eefe0e2f23001d69e3371a61444b06
Daniel Montgomery <damontgomery@gmail.com>
2020-05-11 14:59:52 -0500

    user_load() rule.

---
+++ rector_examples_updated/user_load.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This demonstrates the deprecated static calls that might be called from procedural code like `.module` files.
+ */
+
+/**
+ * A simple example.
+ */
+function simple_example() {
+  /* @var \Drupal\user\Entity\User $user */
+  $user = \Drupal::service('entity_type.manager')->getStorage('user')->load(123);
+}
+
+/**
+ * An example using all of the arguments.
+ */
+function all_arguments() {
+  /* @var \Drupal\user\Entity\User $user */
+  $user = TRUE ? \Drupal::service('entity_type.manager')->getStorage('user')->resetCache([123])->load(123) : \Drupal::service('entity_type.manager')->getStorage('user')->load(123);
+}
+
+/**
+ * An example using all of the arguments as variables.
+ */
+function all_arguments_as_variables() {
+  $entity_id = 123;
+  $reset = TRUE;
+
+  /* @var \Drupal\user\Entity\User $user */
+  $user = $reset ? \Drupal::service('entity_type.manager')->getStorage('user')->resetCache([$entity_id])->load($entity_id) : \Drupal::service('entity_type.manager')->getStorage('user')->load($entity_id);
+}
