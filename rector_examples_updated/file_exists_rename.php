03ab1ea9b352bbc55974744e88cf8105b50d7354
Kelsey Bentham <bentham@palantir.net>
2020-05-21 13:41:03 -0500

    Run rector on updated test files.

--- rector_examples_updated/file_exists_rename.php
+++ rector_examples_updated/file_exists_rename.php
@@ -1,5 +1,6 @@
 <?php
 
+use Drupal\Core\File\FileSystemInterface;
 /**
  * This demonstrates the deprecated static calls that might be called from procedural code like `.module` files.
  */
@@ -8,12 +9,12 @@
  * A simple example.
  */
 function simple_example() {
-  $x = FILE_EXISTS_RENAME;
+  $x = FileSystemInterface::EXISTS_RENAME;
 }
 
 /**
  * An example using the constant as an argument.
  */
 function as_an_argument() {
-  file_unmanaged_copy('/test/directory', '/test/directory/new' . '/file_name.json', FILE_EXISTS_RENAME);
+  file_unmanaged_copy('/test/directory', '/test/directory/new' . '/file_name.json', FileSystemInterface::EXISTS_RENAME);
 }
--- rector_examples_updated/src/FileExistsRenameNoUseStatement.php
+++ rector_examples_updated/src/FileExistsRenameNoUseStatement.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\rector_examples;
 
+use Drupal\Core\File\FileSystemInterface;
 /**
  * Example of method calls from a class without use statements.
  */
@@ -11,14 +12,14 @@ class FileExistsRenameNoUseStatement {
    * A simple example.
    */
   public function simple_example() {
-    $x = FILE_EXISTS_RENAME;
+    $x = FileSystemInterface::EXISTS_RENAME;
   }
 
   /**
    * An example using the constant as an argument.
    */
   public function as_an_argument() {
-    file_unmanaged_copy('/test/directory', '/test/directory/new' . '/file_name.json', FILE_EXISTS_RENAME);
+    file_unmanaged_copy('/test/directory', '/test/directory/new' . '/file_name.json', FileSystemInterface::EXISTS_RENAME);
   }
 
 }
