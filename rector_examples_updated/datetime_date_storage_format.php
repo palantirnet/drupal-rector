1b774540fab3b91860932873a1dfaf6f6dbf7fd0
Derek Wright <git@dwwright.net>
2020-06-03 17:04:19 -1000

    Issue #3145537 by dww: Add rector rule for DATETIME_DATE_STORAGE_FORMAT

---
+++ rector_examples_updated/datetime_date_storage_format.php
@@ -0,0 +1,24 @@
+<?php
+
+use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
+/**
+ * @file
+ * Demonstrates deprecated constants that might be used in procedural code.
+ */
+
+use Drupal\Core\Datetime\DrupalDateTime;
+
+/**
+ * A simple example.
+ */
+function simple_example() {
+  $format = DateTimeItemInterface::DATE_STORAGE_FORMAT;
+}
+
+/**
+ * An example using the constant as an argument.
+ */
+function as_an_argument() {
+  $date = new DrupalDateTime('now', new \DateTimezone('America/Los_Angeles'));
+  $now = $date->format(DateTimeItemInterface::DATE_STORAGE_FORMAT);
+}
