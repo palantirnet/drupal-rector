41e95875699899166a253a23c14f906b82254cb3
Matt Glaman <nmd.matt@gmail.com>
2021-03-29 10:26:14 -0500

    Must add php suffix

--- rector-config-docroot-dir.php
+++ rector-config-docroot-dir.php
@@ -12,7 +12,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters->set(Option::AUTOLOAD_PATHS, ['docroot/core', 'docroot/core/modules', 'docroot/modules', 'docroot/profiles']);
     $parameters->set(Option::SKIP, ['*/upgrade_status/tests/modules/*']);
-    $parameters->set(Option::FILE_EXTENSIONS, ['module', 'theme', 'install', 'profile', 'inc', 'engine']);
+    $parameters->set(Option::FILE_EXTENSIONS, ['php', 'module', 'theme', 'install', 'profile', 'inc', 'engine']);
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
     $parameters->set(Option::IMPORT_SHORT_CLASSES, false);
     $parameters->set(Option::IMPORT_DOC_BLOCKS, false);
