58539fed322ffa3365d1285664eb42b78f20e85d
Daniel Montgomery <damontgomery@gmail.com>
2021-05-03 18:28:59 -0500

    Add back the fix for not finding classes.

--- config/drupal-8/drupal-8-all-deprecations.php
+++ config/drupal-8/drupal-8-all-deprecations.php
@@ -9,4 +9,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/drupal-8.*');
 
     $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::BOOTSTRAP_FILES, [
+        __DIR__ . '/../drupal-phpunit-bootstrap-file.php'
+    ]);
 };
