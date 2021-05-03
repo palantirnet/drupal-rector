48a6b0d6b3f3b60d217c8760009f130da51a2311
Daniel Montgomery <damontgomery@gmail.com>
2021-05-03 15:41:01 -0500

    Remove skip option since it doesn't have any values.

--- config/drupal-8/drupal-8-all-deprecations.php
+++ config/drupal-8/drupal-8-all-deprecations.php
@@ -9,7 +9,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/drupal-8.*');
 
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::SKIP, []);
     $parameters->set(Option::BOOTSTRAP_FILES, [
         __DIR__ . '/../drupal-phpunit-bootstrap-file.php'
     ]);
