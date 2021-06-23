17c85bc7d2652587aa0bae51b1bd6c74bc911adf
Matt Glaman <nmd.matt@gmail.com>
2021-06-23 15:53:02 -0500

    Issue #3217424 by mglaman: Add PHPUnit testing

---
+++ tests/src/DeprecationIndexTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DrupalRector\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Yaml\Yaml;
+
+final class DeprecationIndexTest extends TestCase
+{
+
+    public function testIsValidYaml()
+    {
+        $contents = file_get_contents(__DIR__ . '/../../deprecation-index.yml');
+        self::assertNotFalse($contents);
+        $contents = Yaml::parse($contents);
+        self::assertIsArray($contents);
+    }
+
+    public function testOnlyAsciiCharacters()
+    {
+        $contents = file_get_contents(__DIR__ . '/../../deprecation-index.yml');
+        self::assertNotFalse($contents);
+        $result = mb_detect_encoding($contents, 'ASCII', true);
+        self::assertNotFalse(
+            $result,
+            'The file contains non ASCII characters. Please make sure `\` and spaces are only using ASCII characters.'
+        );
+    }
+
+}
