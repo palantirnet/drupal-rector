9ae26895243397c01913f63c2ea3262de4f0542d
Matt Glaman <nmd.matt@gmail.com>
2021-06-30 15:30:36 -0500

    Add basic rule for drupalPostForm deprecation

---
+++ src/Rector/Deprecation/UiHelperTraitDrupalPostFormRector.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace DrupalRector\Rector\Deprecation;
+
+use PhpParser\Node;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class UiHelperTraitDrupalPostFormRector extends AbstractRector
+{
+
+    public function getRuleDefinition(): RuleDefinition {
+        return new RuleDefinition('Fixes deprecated UiHelperTrait::drupalPostForm() calls',[
+            new CodeSample(
+                <<<'CODE_BEFORE'
+$edit = [];
+$edit['action'] = 'action_goto_action';
+$this->drupalPostForm('admin/config/system/actions', $edit, 'Create');
+CODE_BEFORE
+                ,
+                <<<'CODE_AFTER'
+$edit = [];
+$edit['action'] = 'action_goto_action';
+$this->drupalGet('admin/config/system/actions');
+$this->submitForm($edit, 'Create');
+CODE_AFTER
+            )
+        ]);
+    }
+
+    public function getNodeTypes(): array {
+        return [
+            Node\Expr\MethodCall::class,
+        ];
+    }
+
+    public function refactor(Node $node) {
+        // TODO: Implement refactor() method.
+    }
+}
