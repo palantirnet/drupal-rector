1558ed3c070a0ffe40a97e85ce118be1f31dbc91
Matt Glaman <nmd.matt@gmail.com>
2021-06-30 17:29:37 -0500

    Make the rule pass

--- src/Rector/Deprecation/UiHelperTraitDrupalPostFormRector.php
+++ src/Rector/Deprecation/UiHelperTraitDrupalPostFormRector.php
@@ -4,6 +4,7 @@ namespace DrupalRector\Rector\Deprecation;
 
 use DrupalRector\Utility\AddCommentTrait;
 use PhpParser\Node;
+use Rector\Core\Exception\ShouldNotHappenException;
 use Rector\Core\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -40,21 +41,76 @@ CODE_AFTER
         ];
     }
 
+    /**
+     * @param \PhpParser\Node\Expr\MethodCall $node
+     *
+     * @return \PhpParser\Node\Arg[]
+     * @throws \Rector\Core\Exception\ShouldNotHappenException
+     */
+    private function safeArgDestructure(Node\Expr\MethodCall $node): array
+    {
+        $count = count($node->args);
+        if ($count === 3) {
+            [$path, $edit, $button] = $node->args;
+            return [$path, $edit, $button, null, null];
+        }
+
+        if ($count === 4) {
+            [$path, $edit, $button, $options] = $node->args;
+            return [$path, $edit, $button, $options, null];
+        }
+
+        if ($count === 5) {
+            return $node->args;
+        }
+
+        throw new ShouldNotHappenException('Unexpected argument count for drupalPostForm');
+    }
+
     public function refactor(Node $node): ?Node
     {
         assert($node instanceof Node\Expr\MethodCall);
         if ($this->getName($node->name) === 'drupalPostForm') {
-            // @todo: handle when there are 4 or 5 arugments for opts and ID.
-            [$path, $edit, $button] = $node->args;
+            [$path, $edit, $button, $options, $htmlId] = $this->safeArgDestructure($node);
             // @todo we _must_ inject drupalGet.
             // new Node\Expr\MethodCall($node->var, new Node\Identifier('drupalGet'), [$path])
             $pathValue = $path->value;
             assert($pathValue instanceof Node\Scalar\String_);
-            $this->addDrupalRectorComment(
-                $node,
-                sprintf('You must call `$this->drupalGet("%s");" before submitForm', $pathValue->value)
-            );
-            return new Node\Expr\MethodCall($node->var, new Node\Identifier('submitForm'), [$edit, $button]);
+
+            if ($options === null) {
+                $this->addDrupalRectorComment(
+                    $node,
+                    sprintf('You must call `$this->drupalGet("%s");" before submitForm', $pathValue->value)
+                );
+            } else {
+                if ($options->value instanceof Node\Expr\Array_) {
+                    $optionsArgs = array_map(function (Node\Expr\ArrayItem $item) {
+                        $key = $this->valueResolver->getValue($item->key);
+                        $value = $this->valueResolver->getValue($item->value);
+                        return "'$key' => '$value'";
+                    }, $options->value->items);
+                    $optionsValue = '[' . implode(', ', $optionsArgs) . ']';
+                } elseif ($options->value instanceof Node\Expr\Variable) {
+                    $optionsValue = '$' . $options->value->name;
+                } else {
+                    throw new ShouldNotHappenException(
+                        'Unexpected argument type passed to $options for drupalPostForm'
+                    );
+                }
+                $this->addDrupalRectorComment(
+                    $node,
+                    sprintf(
+                        'You must call `$this->drupalGet("%s", %s);" before submitForm',
+                        $pathValue->value,
+                        $optionsValue
+                    )
+                );
+            }
+
+            if ($htmlId === null) {
+                return new Node\Expr\MethodCall($node->var, new Node\Identifier('submitForm'), [$edit, $button]);
+            }
+            return new Node\Expr\MethodCall($node->var, new Node\Identifier('submitForm'), [$edit, $button, $htmlId]);
         }
         return null;
     }
