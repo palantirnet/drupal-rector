b0544ed4da625c396bb9a5ae0f390fc345ec98d2
Matt Glaman <nmd.matt@gmail.com>
2021-07-14 14:57:28 -0500

    Issue #3222671: assertNotIdentical rule

--- tests/src/Rector/Deprecation/AssertIdenticalRector/config/configured_rule.php
+++ tests/src/Rector/Deprecation/AssertIdenticalRector/config/configured_rule.php
@@ -1,12 +1,14 @@
 <?php declare(strict_types=1);
 
 use DrupalRector\Rector\Deprecation\AssertIdenticalRector;
+use DrupalRector\Rector\Deprecation\AssertNotIdenticalRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     $services->set(AssertIdenticalRector::class);
+    $services->set(AssertNotIdenticalRector::class);
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set('drupal_rector_notices_as_comments', true);
