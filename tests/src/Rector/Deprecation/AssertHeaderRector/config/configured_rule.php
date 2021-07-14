77e6a590bd61aa770c7a6038b24f936619953b35
Matt Glaman <nmd.matt@gmail.com>
2021-07-14 14:30:38 -0500

    Issue #3222671: assertHeader rule

---
+++ tests/src/Rector/Deprecation/AssertHeaderRector/config/configured_rule.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+use DrupalRector\Rector\Deprecation\AssertHeaderRector;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->set(AssertHeaderRector::class);
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set('drupal_rector_notices_as_comments', true);
+};
