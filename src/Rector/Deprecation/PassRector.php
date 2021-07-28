96ede82bfe37c1b0ab7e22d28f94b3d6a61753a4
Matt Glaman <nmd.matt@gmail.com>
2021-07-28 15:19:06 -0500

    Issue #3222671: Rector for pass()

---
+++ src/Rector/Deprecation/PassRector.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace DrupalRector\Rector\Deprecation;
+
+use PhpParser\Node;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+final class PassRector extends AbstractRector
+{
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Fixes deprecated AssertLegacyTrait::assertEqual() calls', [
+            new CodeSample(
+                <<<'CODE_BEFORE'
+$this->pass('The whole transaction is rolled back when a duplicate key insert occurs.');
+CODE_BEFORE
+                ,
+                <<<'CODE_AFTER'
+CODE_AFTER
+            )
+        ]);
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [
+            Node\Expr\MethodCall::class,
+        ];
+    }
+
+    public function refactor(Node $node)
+    {
+        assert($node instanceof Node\Expr\MethodCall);
+        if ($this->getName($node->name) !== 'pass') {
+            return null;
+        }
+
+        $this->removeNode($node);
+        return $node;
+    }
+}
