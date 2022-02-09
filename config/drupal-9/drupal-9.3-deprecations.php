83178f66e99841fc129f6f6959321f00b5df01d4
Matt Glaman <nmd.matt@gmail.com>
2022-02-09 15:21:25 -0600

    Add Rector for deprecated `render`
    Addresses change record https://www.drupal.org/node/2939099

--- config/drupal-9/drupal-9.3-deprecations.php
+++ config/drupal-9/drupal-9.3-deprecations.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use DrupalRector\Rector\Deprecation\DrupalGetFilenameRector;
 use DrupalRector\Rector\Deprecation\DrupalGetPathRector;
+use DrupalRector\Rector\Deprecation\RenderRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -12,4 +13,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     // Change record: https://www.drupal.org/node/2940438.
     $services->set(DrupalGetPathRector::class);
     $services->set(DrupalGetFilenameRector::class);
+
+    // Change record: https://www.drupal.org/node/2939099.
+    $services->set(RenderRector::class);
 };
