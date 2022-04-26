8d564e6851a4b009d0bfe3777a204108f9218e4e
Matt Glaman <nmd.matt@gmail.com>
2022-04-26 13:19:50 -0500

    Fix PHP 8 warnings on `null` to `file_exists`

--- config/drupal-phpunit-bootstrap-file.php
+++ config/drupal-phpunit-bootstrap-file.php
@@ -88,7 +88,7 @@ function drupal_phpunit_contrib_extension_directory_roots($root) {
     $paths[] = is_dir("$path/profiles") ? realpath("$path/profiles") : NULL;
     $paths[] = is_dir("$path/themes") ? realpath("$path/themes") : NULL;
   }
-  return array_filter($paths, 'file_exists');
+  return array_filter(array_filter($paths), 'file_exists');
 }
 
 /**
