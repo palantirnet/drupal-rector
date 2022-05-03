5620cb322a7568541c91ef922e5329a34328ced1
Matt Glaman <matt.glaman@acquia.com>
2022-05-03 15:34:23 -0500

    Add back PHPUnitSetList::PHPUNIT_75; disable auto import

--- config/drupal-8/drupal-8-all-deprecations.php
+++ config/drupal-8/drupal-8-all-deprecations.php
@@ -11,6 +11,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $containerConfigurator->import(PHPUnitSetList::PHPUNIT_60);
     $containerConfigurator->import(PHPUnitSetList::PHPUNIT_70);
+    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_75);
 
     $parameters = $containerConfigurator->parameters();
 
