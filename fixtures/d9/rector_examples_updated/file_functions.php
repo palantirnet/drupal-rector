a94c1fa99c332cfd3269c00ac5db9340df865de6
Claudiu Cristea <clau.cristea@gmail.com>
2022-05-25 11:34:38 +0300

    Issue #3282217: file_build_uri() is deprecated

--- fixtures/d9/rector_examples_updated/file_functions.php
+++ fixtures/d9/rector_examples_updated/file_functions.php
@@ -4,4 +4,7 @@ function file_functions() {
     \Drupal::service('file.repository')->copy();
     \Drupal::service('file.repository')->move();
     \Drupal::service('file.repository')->writeData();
+    $uri1 = \Drupal::service('stream_wrapper_manager')->normalizeUri(\Drupal::config('system.file')->get('default_scheme') . ('://' . 'path/to/file.txt'));
+    $path = 'path/to/other/file.png';
+    $uri2 = \Drupal::service('stream_wrapper_manager')->normalizeUri(\Drupal::config('system.file')->get('default_scheme') . ('://' . $path));
 }
