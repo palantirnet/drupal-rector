698f540f434504e589375154ee422dadcdbbf695
Matt Glaman <matt.glaman@acquia.com>
2023-03-28 10:37:53 -0500

    see if config is even loaded since not bundled in rector

--- config/config.php
+++ config/config.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
+    throw new \RuntimeException('Verifying that config was hit ' . __FILE__);
     $rectorConfig->bootstrapFiles([
         __DIR__ . '/drupal-phpunit-bootstrap-file.php'
     ]);
