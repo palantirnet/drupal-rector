8e42aa67c9e901c71d4113ebe7dfc9efbdbca8b1
bjorn <bjorn@swis.nl>
2023-07-06 17:00:34 +0200

    Make sure we don't change how other tests are run because of new argument in DeprecationBase

--- tests/src/Rector/Deprecation/ConstantToClassConstantRector/config/configured_rule.php
+++ tests/src/Rector/Deprecation/ConstantToClassConstantRector/config/configured_rule.php
@@ -5,7 +5,7 @@ use DrupalRector\Tests\Rector\Deprecation\DeprecationBase;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
-    DeprecationBase::addClass(ConstantToClassConstantRector::class, $rectorConfig, true, [
+    DeprecationBase::addClass(ConstantToClassConstantRector::class, $rectorConfig, false, [
         ConstantToClassConstantRector::DEPRECATED_CONSTANT => 'DATETIME_STORAGE_TIMEZONE',
         ConstantToClassConstantRector::CONSTANT_FULLY_QUALIFIED_CLASS_NAME => 'Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface',
         ConstantToClassConstantRector::CONSTANT => 'STORAGE_TIMEZONE',
