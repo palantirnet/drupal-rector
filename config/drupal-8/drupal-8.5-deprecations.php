3ae23edcec4ec074b788a4d4b081fc2639341ed8
bjorn <bjorn@swis.nl>
2023-07-07 16:22:53 +0200

    Use a ValueObject for configurable rector rule

--- config/drupal-8/drupal-8.5-deprecations.php
+++ config/drupal-8/drupal-8.5-deprecations.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use DrupalRector\Rector\Deprecation\ConstantToClassConstantRector;
 use DrupalRector\Rector\Deprecation\DrupalSetMessageRector;
+use DrupalRector\Rector\ValueObject\ConstantToClass;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -13,35 +14,13 @@ return static function (RectorConfig $rectorConfig): void {
 
 
     /**
-     * Replaces deprecated DATETIME_DATE_STORAGE_FORMAT constant use.
+     * Replaces deprecated DATETIME_DATE_STORAGE_FORMAT, DATETIME_DATETIME_STORAGE_FORMAT, DATETIME_STORAGE_TIMEZONE constant use.
      *
      * See https://www.drupal.org/node/2912980 for change record.
      */
     $rectorConfig->ruleWithConfiguration(ConstantToClassConstantRector::class, [
-        ConstantToClassConstantRector::DEPRECATED_CONSTANT => 'DATETIME_DATE_STORAGE_FORMAT',
-        ConstantToClassConstantRector::CONSTANT_FULLY_QUALIFIED_CLASS_NAME => 'Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface',
-        ConstantToClassConstantRector::CONSTANT => 'DATE_STORAGE_FORMAT',
-    ]);
-
-    /**
-     * Replaces deprecated DATETIME_DATETIME_STORAGE_FORMAT constant use.
-     *
-     * See https://www.drupal.org/node/2912980 for change record.
-     */
-    $rectorConfig->ruleWithConfiguration(ConstantToClassConstantRector::class, [
-        ConstantToClassConstantRector::DEPRECATED_CONSTANT => 'DATETIME_DATETIME_STORAGE_FORMAT',
-        ConstantToClassConstantRector::CONSTANT_FULLY_QUALIFIED_CLASS_NAME => 'Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface',
-        ConstantToClassConstantRector::CONSTANT => 'DATETIME_STORAGE_FORMAT',
-    ]);
-
-    /**
-     * Replaces deprecated DATETIME_STORAGE_TIMEZONE constant use.
-     *
-     * See https://www.drupal.org/node/2912980 for change record.
-     */
-    $rectorConfig->ruleWithConfiguration(ConstantToClassConstantRector::class, [
-        ConstantToClassConstantRector::DEPRECATED_CONSTANT => 'DATETIME_STORAGE_TIMEZONE',
-        ConstantToClassConstantRector::CONSTANT_FULLY_QUALIFIED_CLASS_NAME => 'Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface',
-        ConstantToClassConstantRector::CONSTANT => 'STORAGE_TIMEZONE',
+        new ConstantToClass('DATETIME_DATE_STORAGE_FORMAT', 'Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface', 'DATE_STORAGE_FORMAT'),
+        new ConstantToClass('DATETIME_DATETIME_STORAGE_FORMAT', 'Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface', 'DATETIME_STORAGE_FORMAT'),
+        new ConstantToClass('DATETIME_STORAGE_TIMEZONE', 'Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface', 'STORAGE_TIMEZONE'),
     ]);
 };
