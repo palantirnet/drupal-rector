505ad407aa79b5ae13ad3e6b7f292d88ca9f740b
bjorn <bjorn@swis.nl>
2023-08-06 21:51:28 +0200

    Use DeprecationHelper to create a BC call on a CallLike

--- src/Rector/AbstractDrupalCoreRector.php
+++ src/Rector/AbstractDrupalCoreRector.php
@@ -4,38 +4,39 @@ declare(strict_types=1);
 
 namespace DrupalRector\Rector;
 
+use Drupal\Core\Utility\DeprecationHelper;
 use DrupalRector\Contract\DrupalCoreRectorInterface;
 use PhpParser\Node;
+use PhpParser\NodeDumper;
 use Rector\Core\Rector\AbstractRector;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+use Rector\Php72\NodeFactory\AnonymousFunctionFactory;
 
 abstract class AbstractDrupalCoreRector extends AbstractRector implements DrupalCoreRectorInterface
 {
+
+    public function __construct(
+        private readonly AnonymousFunctionFactory $anonymousFunctionFactory
+    ) {
+    }
 
     public function refactor(Node $node)
     {
         if (version_compare(\Drupal::VERSION, $this->getVersion(), '<')) {
             return null;
         }
+
         $result = $this->doRefactor($node);
 
         if ($result === null) {
             return $result;
         }
-        $versionCompare = $this->nodeFactory->createFuncCall(
-            'version_compare',
-            [
-                $this->nodeFactory->createClassConstFetch(\Drupal::class, 'VERSION'),
-                $this->getVersion(),
-                '>=',
-            ]
-        );
-        $if = new Node\Stmt\If_($versionCompare, [
-            'stmts' => [new Node\Stmt\Expression($result)],
-            'else' => new Node\Stmt\Else_([
-                new Node\Stmt\Expression($node)
-            ]),
-        ]);
-        $this->nodesToAddCollector->addNodeBeforeNode($if, $node);
+
+        if($node instanceof Node\Expr\CallLike && $result instanceof Node\Expr\CallLike) {
+            $bcCall = $this->addBackwardsCompatibleCall($node, $result);
+            return $bcCall;
+        }
+
         return $result;
     }
 
@@ -44,5 +45,16 @@ abstract class AbstractDrupalCoreRector extends AbstractRector implements Drupal
      * @return Node|Node[]|null
      */
     abstract protected function doRefactor(Node $node);
+
+    private function addBackwardsCompatibleCall(Node\Expr\CallLike $node, Node\Expr\CallLike $result): Node\Expr\StaticCall
+    {
+        $clonedNode = clone $node;
+        return $this->nodeFactory->createStaticCall(DeprecationHelper::class, 'backwardsCompatibleCall', [
+            $this->nodeFactory->createClassConstFetch(\Drupal::class, 'VERSION'),
+            $this->getVersion(),
+            $this->anonymousFunctionFactory->create([], [new Node\Stmt\Return_($clonedNode)], null),
+            $this->anonymousFunctionFactory->create([], [new Node\Stmt\Return_($result)], null),
+        ]);
+    }
 
 }
