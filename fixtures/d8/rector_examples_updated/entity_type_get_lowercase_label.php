554f3d79fa7b2a4a2a5a71fc35af846a869e053e
bjorn <bjorn@swis.nl>
2023-08-23 09:03:03 +0200

    Fix broken expectation for d8 fixture.
    It seems the analysis got better in a way Changing to logical expected class name. and added to unit tests.

--- fixtures/d8/rector_examples_updated/entity_type_get_lowercase_label.php
+++ fixtures/d8/rector_examples_updated/entity_type_get_lowercase_label.php
@@ -12,7 +12,7 @@ function simple_example() {
   $node = \Drupal::entityTypeManager()->getStorage('node')->load(123);
   $entity_type = $node->getEntityType();
   // TODO: Drupal Rector Notice: Please delete the following comment after you've made any necessary changes.
-  // Please confirm that `$entity_type` is an instance of `\Drupal\Core\Entity\EntityType`. Only the method name and not the class name was checked for this replacement, so this may be a false positive.
+  // Please confirm that `$entity_type` is an instance of `Drupal\Core\Entity\EntityTypeInterface`. Only the method name and not the class name was checked for this replacement, so this may be a false positive.
   $entity_type->getSingularLabel();
 }
 
@@ -25,6 +25,6 @@ function chained_example() {
   /* @var \Drupal\node\Entity\Node $node */
   $node = \Drupal::entityTypeManager()->getStorage('node')->load(123);
   // TODO: Drupal Rector Notice: Please delete the following comment after you've made any necessary changes.
-  // Please confirm that `getEntityType()` is an instance of `\Drupal\Core\Entity\EntityType`. Only the method name and not the class name was checked for this replacement, so this may be a false positive.
+  // Please confirm that `getEntityType()` is an instance of `Drupal\Core\Entity\EntityTypeInterface`. Only the method name and not the class name was checked for this replacement, so this may be a false positive.
   $label = $node->getEntityType()->getSingularLabel();
 }
