25af34d6c99f3fa5fb04daa39fd13f39d9ef852b
bjorn <bjorn@swis.nl>
2023-09-01 15:30:07 +0200

    Rector config screwup with empty methodcall

--- rector.php
+++ rector.php
@@ -27,8 +27,6 @@ return static function (RectorConfig $rectorConfig): void {
         $drupalRoot . '/themes'
     ]);
 
-    $rectorConfig->
-
     $rectorConfig->skip(['*/upgrade_status/tests/modules/*']);
     $rectorConfig->fileExtensions(['php', 'module', 'theme', 'install', 'profile', 'inc', 'engine']);
     $rectorConfig->importNames(true, false);
