0494f979b0b6365e52b232e025935fa619e5f4a0
bjorn <bjorn@swis.nl>
2023-09-06 20:55:38 +0200

    Remove duplicate leversetlists from 10.1 deprecations.

--- config/drupal-10/drupal-10.1-deprecations.php
+++ config/drupal-10/drupal-10.1-deprecations.php
@@ -11,9 +11,7 @@ use Rector\Symfony\Set\TwigLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
-        PHPUnitLevelSetList::UP_TO_PHPUNIT_90,
         SymfonyLevelSetList::UP_TO_SYMFONY_63,
-        TwigLevelSetList::UP_TO_TWIG_240,
     ]);
 
     // https://www.drupal.org/node/3244583
