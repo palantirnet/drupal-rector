44700d72a79767edc30e6d64b92946099dca6845
bjorn <bjorn@swis.nl>
2023-09-08 12:23:19 +0200

    Start working on https://github.com/mglaman/drupal-change-record-triage/issues/282, multiple taxonomy functions.

---
+++ tests/src/Rector/Deprecation/TaxonomyRectorCollection/config/configured_rule.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+use DrupalRector\Rector\Deprecation\FunctionToEntityTypeStorageMethod;
+use DrupalRector\Rector\ValueObject\FunctionToEntityTypeStorageConfiguration;
+use DrupalRector\Tests\Rector\Deprecation\DeprecationBase;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    DeprecationBase::addClass(FunctionToEntityTypeStorageMethod::class, $rectorConfig, FALSE, [
+        new FunctionToEntityTypeStorageConfiguration('taxonomy_terms_static_reset', 'taxonomy_term', 'resetCache'),
+        new FunctionToEntityTypeStorageConfiguration('taxonomy_vocabulary_static_reset', 'taxonomy_vocabulary', 'resetCache'),
+    ]);
+};
