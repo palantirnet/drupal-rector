22331070d6319040b0a98e5270b5668e293a404d
bjorn <bjorn@swis.nl>
2023-09-08 12:46:38 +0200

    Rector for drupal_static_reset('taxonomy_vocabulary_get_names');

--- tests/src/Rector/Deprecation/TaxonomyRectorCollection/config/configured_rule.php
+++ tests/src/Rector/Deprecation/TaxonomyRectorCollection/config/configured_rule.php
@@ -2,6 +2,7 @@
 
 use DrupalRector\Rector\Deprecation\FunctionToEntityTypeStorageMethod;
 use DrupalRector\Rector\Deprecation\TaxonomyTermLoadMultipleByNameRector;
+use DrupalRector\Rector\Deprecation\TaxonomyVocabularyGetNamesDrupalStaticResetRector;
 use DrupalRector\Rector\Deprecation\TaxonomyVocabularyGetNamesRector;
 use DrupalRector\Rector\ValueObject\FunctionToEntityTypeStorageConfiguration;
 use DrupalRector\Tests\Rector\Deprecation\DeprecationBase;
@@ -15,4 +16,5 @@ return static function (RectorConfig $rectorConfig): void {
 
     DeprecationBase::addClass(TaxonomyVocabularyGetNamesRector::class, $rectorConfig, FALSE);
     DeprecationBase::addClass(TaxonomyTermLoadMultipleByNameRector::class, $rectorConfig, FALSE);
+    DeprecationBase::addClass(TaxonomyVocabularyGetNamesDrupalStaticResetRector::class, $rectorConfig, FALSE);
 };
