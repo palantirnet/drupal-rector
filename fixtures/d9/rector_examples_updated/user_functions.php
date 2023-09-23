c0c273f3994629aaed7e58ebb193703e4c19c2c7
bjorn <bjorn@swis.nl>
2023-09-23 08:58:32 +0200

    Fix functional test for user_password

--- fixtures/d9/rector_examples_updated/user_functions.php
+++ fixtures/d9/rector_examples_updated/user_functions.php
@@ -3,8 +3,8 @@
 use Drupal\Component\Utility\DeprecationHelper;
 function user_functions() {
     // user_password().
-    $password = DeprecationHelper::backwardsCompatibleCall(\Drupal::VERSION, '9.1.0', fn() => user_password(), fn() => \Drupal::service('password_generator')->generate());
-    $other_password = DeprecationHelper::backwardsCompatibleCall(\Drupal::VERSION, '9.1.0', fn() => user_password(8), fn() => \Drupal::service('password_generator')->generate(8));
+    $password = \Drupal::service('password_generator')->generate();
+    $other_password = \Drupal::service('password_generator')->generate(8);
     $password_length = 12;
-    $last_password = DeprecationHelper::backwardsCompatibleCall(\Drupal::VERSION, '9.1.0', fn() => user_password($password_length), fn() => \Drupal::service('password_generator')->generate($password_length));
+    $last_password = \Drupal::service('password_generator')->generate($password_length);
 }
