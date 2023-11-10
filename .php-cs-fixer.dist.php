4b505dc20dbd28d616c3a61b2553b792bad9bf8f
bjorn <bjorn@swis.nl>
2023-11-10 20:30:59 +0100

    Add php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,25 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+        __DIR__.'/config/drupal-*',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'linebreak_after_opening_tag' => true,
+        'ordered_imports' => true,
+        'phpdoc_add_missing_param_annotation' => true,
+        'phpdoc_order' => true,
+        'yoda_style' => false,
+        'no_superfluous_phpdoc_tags' => false,
+        'declare_strict_types' => true,
+    ])
+    ->setFinder($finder)
+    ;
