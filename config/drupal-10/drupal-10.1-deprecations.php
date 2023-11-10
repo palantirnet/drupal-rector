8f6bfd8b8b4e786e0c04b96f4ec735fe91010168
bjorn <bjorn@swis.nl>
2023-11-10 16:44:12 +0100

    Add WatchdogExceptionRector the the 10.1 setlist

--- config/drupal-10/drupal-10.1-deprecations.php
+++ config/drupal-10/drupal-10.1-deprecations.php
@@ -3,11 +3,11 @@
 declare(strict_types=1);
 
 use DrupalRector\Rector\Deprecation\FunctionToStaticRector;
+use DrupalRector\Rector\Deprecation\WatchdogExceptionRector;
+use DrupalRector\Rector\ValueObject\DrupalIntroducedVersionConfiguration;
 use DrupalRector\Rector\ValueObject\FunctionToStaticConfiguration;
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\Symfony\Set\SymfonyLevelSetList;
-use Rector\Symfony\Set\TwigLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
@@ -17,5 +17,10 @@ return static function (RectorConfig $rectorConfig): void {
     // https://www.drupal.org/node/3244583
     $rectorConfig->ruleWithConfiguration(FunctionToStaticRector::class, [
         new FunctionToStaticConfiguration('10.1.0', 'drupal_rewrite_settings', 'Drupal\Core\Site\SettingsEditor', 'rewrite', [0 => 1, 1 => 0]),
+    ]);
+
+    // https://www.drupal.org/node/2932520
+    $rectorConfig->ruleWithConfiguration(WatchdogExceptionRector::class, [
+        new DrupalIntroducedVersionConfiguration('10.1.0'),
     ]);
 };
