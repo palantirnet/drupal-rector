010e25636354a981d3865e4ef09333539be06df2
bjorn <bjorn@swis.nl>
2023-12-10 09:47:16 +0100

    Fix namespace in config for ActionAnnotationToAttributeRector

--- config/drupal-10/drupal-10.2-deprecations.php
+++ config/drupal-10/drupal-10.2-deprecations.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
+use DrupalRector\Drupal10\Rector\Deprecation\ActionAnnotationToAttributeRector;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     // @see https://www.drupal.org/node/3395575
-    $rectorConfig->rule(\DrupalRector\Rector\Deprecation\ActionAnnotationToAttributeRector::class);
+    $rectorConfig->rule(ActionAnnotationToAttributeRector::class);
 };
