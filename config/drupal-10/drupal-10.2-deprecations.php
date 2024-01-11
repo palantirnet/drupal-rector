b477f966de76bc7df3b86cd98d1144906cffe1b9
Timo Huisman <thuisman@swis.nl>
2024-01-11 19:58:42 +0100

    refactor: remove duplicated SymfonyLevelSetList

--- config/drupal-10/drupal-10.2-deprecations.php
+++ config/drupal-10/drupal-10.2-deprecations.php
@@ -5,13 +5,8 @@ declare(strict_types=1);
 use DrupalRector\Rector\Deprecation\FunctionToStaticRector;
 use DrupalRector\Rector\ValueObject\FunctionToStaticConfiguration;
 use Rector\Config\RectorConfig;
-use Rector\Symfony\Set\SymfonyLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([
-        SymfonyLevelSetList::UP_TO_SYMFONY_63,
-    ]);
-
     // https://www.drupal.org/node/2999981
     $rectorConfig->ruleWithConfiguration(FunctionToStaticRector::class, [
         new FunctionToStaticConfiguration('10.2.0', 'format_size', '\Drupal\Core\StringTranslation\ByteSizeMarkup', 'create'),
