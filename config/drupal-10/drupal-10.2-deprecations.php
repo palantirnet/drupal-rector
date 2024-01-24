951d7b7fce8469fd068f5676399d2d93116e34fa
Timo Huisman <thuisman@swis.nl>
2024-01-24 17:00:16 +0100

    feat: add rector rule for deprecated GDToolkit resource methods and properties in 10.2

--- config/drupal-10/drupal-10.2-deprecations.php
+++ config/drupal-10/drupal-10.2-deprecations.php
@@ -3,12 +3,27 @@
 declare(strict_types=1);
 
 use DrupalRector\Rector\Deprecation\FunctionToStaticRector;
+use DrupalRector\Rector\Deprecation\MethodToMethodWithCheckRector;
 use DrupalRector\Rector\ValueObject\FunctionToStaticConfiguration;
+use DrupalRector\Rector\ValueObject\MethodToMethodWithCheckConfiguration;
 use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\PropertyFetch\RenamePropertyRector;
+use Rector\Renaming\ValueObject\RenameProperty;
 
 return static function (RectorConfig $rectorConfig): void {
     // https://www.drupal.org/node/2999981
     $rectorConfig->ruleWithConfiguration(FunctionToStaticRector::class, [
         new FunctionToStaticConfiguration('10.2.0', 'format_size', '\Drupal\Core\StringTranslation\ByteSizeMarkup', 'create'),
+    ]);
+
+    // https://www.drupal.org/node/3265963
+    $rectorConfig->ruleWithConfiguration(MethodToMethodWithCheckRector::class, [
+        new MethodToMethodWithCheckConfiguration('Drupal\system\Plugin\ImageToolkit\GDToolkit', 'getResource', 'getImage'),
+        new MethodToMethodWithCheckConfiguration('Drupal\system\Plugin\ImageToolkit\GDToolkit', 'setResource', 'setImage'),
+    ]);
+
+    // https://www.drupal.org/node/3265963
+    $rectorConfig->ruleWithConfiguration(RenamePropertyRector::class, [
+        new RenameProperty('Drupal\system\Plugin\ImageToolkit\GDToolkit', 'resource', 'image'),
     ]);
 };
