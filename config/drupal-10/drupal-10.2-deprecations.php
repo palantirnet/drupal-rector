18a3ea50be2fe8b023b45a865f59d0fce30897cf
Timo Huisman <thuisman@swis.nl>
2024-03-04 08:49:01 +0100

    feat: Add rector rule for deprecated GDToolkit  (#289)
    * feat: add rector rule for deprecated GDToolkit resource methods and properties in 10.2
    
    * fix: remove renaming ::resource since we see no usage for that and its protected anyways.
    
    * fix: style fixes
    
    ---------
    
    Co-authored-by: bjorn <bjorn@swis.nl>

--- config/drupal-10/drupal-10.2-deprecations.php
+++ config/drupal-10/drupal-10.2-deprecations.php
@@ -3,12 +3,20 @@
 declare(strict_types=1);
 
 use DrupalRector\Rector\Deprecation\FunctionToStaticRector;
+use DrupalRector\Rector\Deprecation\MethodToMethodWithCheckRector;
 use DrupalRector\Rector\ValueObject\FunctionToStaticConfiguration;
+use DrupalRector\Rector\ValueObject\MethodToMethodWithCheckConfiguration;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     // https://www.drupal.org/node/2999981
     $rectorConfig->ruleWithConfiguration(FunctionToStaticRector::class, [
         new FunctionToStaticConfiguration('10.2.0', 'format_size', '\Drupal\Core\StringTranslation\ByteSizeMarkup', 'create'),
+    ]);
+
+    // https://www.drupal.org/node/3265963
+    $rectorConfig->ruleWithConfiguration(MethodToMethodWithCheckRector::class, [
+        new MethodToMethodWithCheckConfiguration('Drupal\system\Plugin\ImageToolkit\GDToolkit', 'getResource', 'getImage'),
+        new MethodToMethodWithCheckConfiguration('Drupal\system\Plugin\ImageToolkit\GDToolkit', 'setResource', 'setImage'),
     ]);
 };
