27e1ef5419e72f007c92d2288f91cd28e99d1628
Björn Brala <bjorn@swis.nl>
2024-03-05 20:37:18 +0100

    fix: Add proper levels for all symfony versions deprecated in the different versions of Drupal (#293)

--- config/drupal-10/drupal-10.0-deprecations.php
+++ config/drupal-10/drupal-10.0-deprecations.php
@@ -11,6 +11,13 @@ use Rector\Symfony\Set\TwigSetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         PHPUnitSetList::PHPUNIT_90,
+        SymfonySetList::SYMFONY_50,
+        SymfonySetList::SYMFONY_51,
+        SymfonySetList::SYMFONY_52,
+        SymfonySetList::SYMFONY_53,
+        SymfonySetList::SYMFONY_54,
+        SymfonySetList::SYMFONY_60,
+        SymfonySetList::SYMFONY_61,
         SymfonySetList::SYMFONY_62,
         TwigSetList::TWIG_240,
     ]);
--- config/drupal-10/drupal-10.2-deprecations.php
+++ config/drupal-10/drupal-10.2-deprecations.php
@@ -7,8 +7,13 @@ use DrupalRector\Rector\Deprecation\MethodToMethodWithCheckRector;
 use DrupalRector\Rector\ValueObject\FunctionToStaticConfiguration;
 use DrupalRector\Rector\ValueObject\MethodToMethodWithCheckConfiguration;
 use Rector\Config\RectorConfig;
+use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([
+        SymfonySetList::SYMFONY_64,
+    ]);
+
     // https://www.drupal.org/node/2999981
     $rectorConfig->ruleWithConfiguration(FunctionToStaticRector::class, [
         new FunctionToStaticConfiguration('10.2.0', 'format_size', '\Drupal\Core\StringTranslation\ByteSizeMarkup', 'create'),
