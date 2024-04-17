a0e43bdf7de42fb99d8ba4327f63118056b275b2
bjorn <bjorn@swis.nl>
2024-04-17 23:59:55 +0200

    fix: remove extra \ from Drupal\Core\StringTranslation\ByteSizeMarkup

--- tests/src/Rector/Deprecation/FunctionToStaticRector/config/configured_rule.php
+++ tests/src/Rector/Deprecation/FunctionToStaticRector/config/configured_rule.php
@@ -11,6 +11,6 @@ return static function (RectorConfig $rectorConfig): void {
     DeprecationBase::addClass(FunctionToStaticRector::class, $rectorConfig, false, [
         new FunctionToStaticConfiguration('8.1.0', 'file_directory_os_temp', 'Drupal\Component\FileSystem\FileSystem', 'getOsTemporaryDirectory'),
         new FunctionToStaticConfiguration('10.1.0', 'drupal_rewrite_settings', 'Drupal\Core\Site\SettingsEditor', 'rewrite', [0 => 1, 1 => 0]),
-        new FunctionToStaticConfiguration('10.2.0', 'format_size', '\Drupal\Core\StringTranslation\ByteSizeMarkup', 'create'),
+        new FunctionToStaticConfiguration('10.2.0', 'format_size', 'Drupal\Core\StringTranslation\ByteSizeMarkup', 'create'),
     ]);
 };
