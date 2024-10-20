47261eeb115f4131db3b6059a230fc81c0de2b2c
bjorn <bjorn@swis.nl>
2024-10-20 11:09:22 +0200

    feat: remove Drupal 8 rules from default set

--- rector.php
+++ rector.php
@@ -11,8 +11,9 @@ return static function (RectorConfig $rectorConfig): void {
     // Adjust the set lists to be more granular to your Drupal requirements.
     // @todo find out how to only load the relevant rector rules.
     //   Should we try and load \Drupal::VERSION and check?
+    //   new possible option with ComposerTriggeredSet
+    //   https://github.com/rectorphp/rector-src/blob/b5a5739b7d7dde621053adff113449860ed5331f/src/Set/ValueObject/ComposerTriggeredSet.php
     $rectorConfig->sets([
-        Drupal8SetList::DRUPAL_8,
         Drupal9SetList::DRUPAL_9,
         Drupal10SetList::DRUPAL_10,
     ]);
