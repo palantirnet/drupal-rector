ccbf25077ad37544644384c7af8edd36d4784dc8
bjorn <bjorn@swis.nl>
2024-10-20 12:52:03 +0200

    feat: initial new test structure

---
+++ tests/functional/HookConverRector/rector.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use DrupalFinder\DrupalFinder;
+use DrupalRector\Set\Drupal10SetList;
+use DrupalRector\Set\Drupal8SetList;
+use DrupalRector\Set\Drupal9SetList;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rule(\DrupalRector\Rector\Convert\HookConvertRector::class);
+
+    $drupalFinder = new DrupalFinder();
+    $drupalFinder->locateRoot(__DIR__);
+    $drupalRoot = $drupalFinder->getDrupalRoot();
+    $rectorConfig->autoloadPaths([
+        $drupalRoot . '/core',
+        $drupalRoot . '/modules',
+        $drupalRoot . '/profiles',
+        $drupalRoot . '/themes'
+    ]);
+
+    $rectorConfig->skip(['*/upgrade_status/tests/modules/*']);
+    $rectorConfig->fileExtensions(['php', 'module', 'theme', 'install', 'profile', 'inc', 'engine']);
+    $rectorConfig->importNames(true, false);
+    $rectorConfig->importShortClasses(false);
+};
