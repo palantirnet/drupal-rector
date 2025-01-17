3cdcc87ed16592cf567e870ef49a7b93cc3c2c62
bjorn <bjorn@swis.nl>
2025-01-17 17:01:06 +0100

    fix: update expected

--- tests/functional/hookconvertrector/fixture/hookconvertrector_updated/src/Hook/HookconvertrectorHooks.php
+++ tests/functional/hookconvertrector/fixture/hookconvertrector_updated/src/Hook/HookconvertrectorHooks.php
@@ -11,8 +11,8 @@ class HookconvertrectorHooks
     /**
      * Implements hook_user_cancel().
      */
-    #[Hook('user_cancel', module: 'module')]
-    public function moduleUserCancel($edit, \UserInterface $account, $method)
+    #[Hook('user_cancel')]
+    public function userCancel($edit, \UserInterface $account, $method)
     {
         $red = 'red';
     }
