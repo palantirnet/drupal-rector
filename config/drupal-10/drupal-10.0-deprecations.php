f0c7aa10a48e53d52381776ecc7c288ef9461f96
Björn Brala <github@bjorn.dev>
2025-05-05 09:16:27 +0200

    Update config/drupal-10/drupal-10.0-deprecations.php
    Co-authored-by: DavorHorvacki <42215392+Holo96@users.noreply.github.com>

--- config/drupal-10/drupal-10.0-deprecations.php
+++ config/drupal-10/drupal-10.0-deprecations.php
@@ -9,11 +9,13 @@ use Rector\Symfony\Set\SymfonySetList;
 use Rector\Symfony\Set\TwigSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    if (defined('TwigSetList::TWIG_24')) {
-        $twigSet = TwigSetList::TWIG_24;
-    } else {
-        $twigSet = TwigSetList::TWIG_240;
-    }
+if (defined(TwigSetList::class .'::TWIG_24')) {
+    $twigSet = TwigSetList::TWIG_24;
+  } elseif (defined(TwigSetList::class .'::TWIG_240'))  {
+    $twigSet = TwigSetList::TWIG_240;
+  } else {
+    throw new ShouldNotHappenException('Could not detect twig set.');
+  }
 
     $rectorConfig->sets([
         PHPUnitSetList::PHPUNIT_90,
