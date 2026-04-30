aa4c878a9003d3c206e00492d8f360a551074b3e
bjorn <bjorn@swis.nl>
2026-04-30 15:57:38 +0200

    feat(Drupal11): Add ReplaceCommentManagerGetCountNewCommentsRector for issue #3543035
    Replaces CommentManagerInterface::getCountNewComments() with
    \Drupal::service(\Drupal\history\HistoryManager::class)->getCountNewComments(),
    BC-wrapped for drupal:11.3.0.

--- config/drupal-11/drupal-11.3-deprecations.php
+++ config/drupal-11/drupal-11.3-deprecations.php
@@ -2,11 +2,20 @@
 
 declare(strict_types=1);
 
+use DrupalRector\Drupal11\Rector\Deprecation\ReplaceCommentManagerGetCountNewCommentsRector;
 use DrupalRector\Rector\Deprecation\FunctionToServiceRector;
+use DrupalRector\Rector\ValueObject\DrupalIntroducedVersionConfiguration;
 use DrupalRector\Rector\ValueObject\FunctionToServiceConfiguration;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
+    // https://www.drupal.org/node/3543035
+    // CommentManagerInterface::getCountNewComments() deprecated in drupal:11.3.0, removed in drupal:12.0.0.
+    // Replaced by \Drupal\history\HistoryManager::getCountNewComments().
+    $rectorConfig->ruleWithConfiguration(ReplaceCommentManagerGetCountNewCommentsRector::class, [
+        new DrupalIntroducedVersionConfiguration('11.3.0'),
+    ]);
+
     // https://www.drupal.org/node/3571623
     // node_mass_update() deprecated in drupal:11.3.0, removed in drupal:13.0.0.
     // Replaced by \Drupal\node\NodeBulkUpdate::process().
---
+++ src/Drupal11/Rector/Deprecation/ReplaceCommentManagerGetCountNewCommentsRector.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrupalRector\Drupal11\Rector\Deprecation;
+
+use DrupalRector\Contract\VersionedConfigurationInterface;
+use DrupalRector\Rector\AbstractDrupalCoreRector;
+use DrupalRector\Rector\ValueObject\DrupalIntroducedVersionConfiguration;
+use PhpParser\Node;
+use PHPStan\Type\ObjectType;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\ConfiguredCodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * Replaces deprecated CommentManagerInterface::getCountNewComments() calls.
+ *
+ * @see https://www.drupal.org/node/3543035
+ */
+final class ReplaceCommentManagerGetCountNewCommentsRector extends AbstractDrupalCoreRector
+{
+    /**
+     * @var array|DrupalIntroducedVersionConfiguration[]
+     */
+    protected array $configuration;
+
+    public function configure(array $configuration): void
+    {
+        foreach ($configuration as $value) {
+            if (!$value instanceof DrupalIntroducedVersionConfiguration) {
+                throw new \InvalidArgumentException(sprintf('Each configuration item must be an instance of "%s"', DrupalIntroducedVersionConfiguration::class));
+            }
+        }
+
+        parent::configure($configuration);
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [Node\Expr\MethodCall::class];
+    }
+
+    public function refactorWithConfiguration(Node $node, VersionedConfigurationInterface $configuration): ?Node
+    {
+        if (!$node instanceof Node\Expr\MethodCall) {
+            return null;
+        }
+
+        if (!$this->isName($node->name, 'getCountNewComments')) {
+            return null;
+        }
+
+        if (!$this->isObjectType($node->var, new ObjectType('Drupal\comment\CommentManagerInterface'))) {
+            return null;
+        }
+
+        $service = new Node\Expr\StaticCall(
+            new Node\Name\FullyQualified('Drupal'),
+            'service',
+            [new Node\Arg(new Node\Expr\ClassConstFetch(new Node\Name\FullyQualified('Drupal\history\HistoryManager'), 'class'))]
+        );
+
+        return new Node\Expr\MethodCall($service, 'getCountNewComments', $node->args);
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Replaces deprecated CommentManagerInterface::getCountNewComments() with HistoryManager service', [
+            new ConfiguredCodeSample(
+                <<<'CODE_BEFORE'
+$this->commentManager->getCountNewComments($entity);
+CODE_BEFORE,
+                <<<'CODE_AFTER'
+\Drupal::service(\Drupal\history\HistoryManager::class)->getCountNewComments($entity);
+CODE_AFTER,
+                [new DrupalIntroducedVersionConfiguration('11.3.0')]
+            ),
+        ]);
+    }
+}
