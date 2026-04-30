b26eb2a1f92c70a378f17a40b3eac48f43a60323
bjorn <bjorn@swis.nl>
2026-04-30 15:01:01 +0200

    feat(Drupal10): Add FileSystemInterface::EXISTS_* → FileExists enum replacements
    FileSystemInterface::EXISTS_RENAME, EXISTS_REPLACE, and EXISTS_ERROR were
    deprecated in drupal:10.3.0 and removed in drupal:12.0.0 (issue #3575575).
    Replaced by the \Drupal\Core\File\FileExists backed enum.

--- config/drupal-10/drupal-10.3-deprecations.php
+++ config/drupal-10/drupal-10.3-deprecations.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use DrupalRector\Rector\Deprecation\ClassConstantToClassConstantRector;
 use DrupalRector\Rector\Deprecation\FunctionToStaticRector;
+use DrupalRector\Rector\ValueObject\ClassConstantToClassConstantConfiguration;
 use DrupalRector\Rector\ValueObject\FunctionToStaticConfiguration;
 use Rector\Config\RectorConfig;
 
@@ -11,5 +13,29 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(FunctionToStaticRector::class, [
         new FunctionToStaticConfiguration('10.3.0', 'file_icon_class', 'Drupal\file\IconMimeTypes', 'getIconClass'),
         new FunctionToStaticConfiguration('10.3.0', 'file_icon_map', 'Drupal\file\IconMimeTypes', 'getGenericMimeType'),
+    ]);
+
+    // https://www.drupal.org/node/3575575
+    // FileSystemInterface::EXISTS_* deprecated in drupal:10.3.0, removed in drupal:12.0.0.
+    // Replaced by \Drupal\Core\File\FileExists enum cases.
+    $rectorConfig->ruleWithConfiguration(ClassConstantToClassConstantRector::class, [
+        new ClassConstantToClassConstantConfiguration(
+            'Drupal\Core\File\FileSystemInterface',
+            'EXISTS_RENAME',
+            'Drupal\Core\File\FileExists',
+            'Rename',
+        ),
+        new ClassConstantToClassConstantConfiguration(
+            'Drupal\Core\File\FileSystemInterface',
+            'EXISTS_REPLACE',
+            'Drupal\Core\File\FileExists',
+            'Replace',
+        ),
+        new ClassConstantToClassConstantConfiguration(
+            'Drupal\Core\File\FileSystemInterface',
+            'EXISTS_ERROR',
+            'Drupal\Core\File\FileExists',
+            'Error',
+        ),
     ]);
 };
