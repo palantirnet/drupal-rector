d641f308daadfb814431aa6834b02d4a7ec7392b
bjorn <bjorn@swis.nl>
2026-04-30 11:39:56 +0200

    feat: Add Drupal11 directory scaffold and SetList stub
    Creates src/Drupal11/Rector/Deprecation/ and tests/src/Drupal11/Rector/Deprecation/
    directory structure for incoming Drupal 11 deprecation rules. Adds stub
    Drupal11SetList.php following the established Drupal10SetList pattern.

---
+++ src/Set/Drupal11SetList.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrupalRector\Set;
+
+final class Drupal11SetList
+{
+    public const DRUPAL_11 = __DIR__.'/../../config/drupal-11/drupal-11-all-deprecations.php';
+}
